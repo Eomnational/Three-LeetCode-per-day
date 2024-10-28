0f9a2070b9744704102570e2ddb51a86bc16a43d
xunana <3520653564@qq.com>
2024-10-28 20:30:02 +0800

    feat:35. 搜索插入位置10.28

---
+++ 6.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let mid,left=0,right=nums.length-1;
+    while(left<=right){
+        mid=left+((right-left)>>1)
+
+        if(nums[mid]>target){
+            right=mid-1;
+        }else if(nums[mid]<target){
+            left=mid+1;
+        }else{
+            return mid;
+        }
+    }
+    return right+1;
+
+};
